13ded7ed595f531208faad97b59fefd3139524fa
Michael Rusz <arembi@protonmail.com>
2021-03-06 11:13:01 +0100

    Naming convention update

--- core/settings.php
+++ core/settings.php
@@ -6,27 +6,24 @@
 namespace Arembi\Xfw\Core;
 
 class Settings {
-	private static $settings = [];
+	private static $settings;
 
-	private static $model = null;
+	private static $model;
 
 
 	public static function init()
 	{
+		self::$settings = [];
+		self::$model = null;
+
 		$domain = Router::getDomainByID(DOMAIN_ID);
 		$dbSettings = $domain['settings'] ?? [];
 		self::$settings = array_merge(Config::_('defaultDomainSettings'), $dbSettings);
 	}
 
 
-	public static function _($record)
+	public static function get($record)
 	{
 		return isset(self::$settings[$record]) ? self::$settings[$record] : NULL;
-	}
-
-
-	public static function read($record)
-	{
-		return self::_($record);
 	}
 }
