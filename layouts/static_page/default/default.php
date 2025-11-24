656ee72a71796ffcff445f1a267d69c1c64b80c8
vscode <arembi@protonmail.com>
2025-11-24 22:03:57 +0100

    replaced echo with $this->print

--- layouts/static_page/default/default.php
+++ layouts/static_page/default/default.php
@@ -1,10 +1,10 @@
 <div class="staticPage">
 	<header>
 		<h1><?php $this->print($title); ?></h1>
-		<div><?php $this->print(['en'=> 'Created at:']); ?> <time><?php echo $createdAt ?></time></div>
-		<div><?php $this->print(['en'=> 'Last updated:']); ?> <time><?php echo $updatedAt ?></time></div>
+		<div><?php $this->print(['en'=> 'Created at:']); ?> <time><?php $this->print($createdAt) ?></time></div>
+		<div><?php $this->print(['en'=> 'Last updated:']); ?> <time><?php $this->print($updatedAt) ?></time></div>
 	</header>
 	<section>
-		<?php echo $content ?>
+		<?php $this->print($content); ?>
 	</section>
 </div>
