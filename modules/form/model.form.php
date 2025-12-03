de2a5e106148c3d865678116b0bfc84ccae9ddf6
vscode <arembi@protonmail.com>
2025-12-03 14:05:48 +0100

    updated Form data collection

--- modules/form/model.form.php
+++ modules/form/model.form.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace Arembi\Xfw\Module;
+
 use Arembi\Xfw\Core\Models\Form;
 
 class FormBaseModel {
 
-	public function getForm(int $formId)
+	public function getFormById(int $formId)
 	{
 		return Form::find($formId);
+	}
+
+
+	public function getFormByName(string $name)
+	{
+		return Form::where('name', $name)->first();
 	}
 
 }
