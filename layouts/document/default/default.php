d27e549e685d55a5e6b42f68c509b5d48c4f83e0
vscode <arembi@protonmail.com>
2025-12-05 09:42:15 +0100

    removed body_start and body_end modules from the app

--- layouts/document/default/default.php
+++ layouts/document/default/default.php
@@ -2,9 +2,7 @@
 <html lang="<?php $this->print($lang);?>"><head>
   <?php $this->embed('head');?>
 </head><body>
-  <?php $this->embed('body_start');?>
   <main>
     <?php $this->embed($primaryModule, $primaryModuleParameters)?>
   </main>
-  <?php $this->embed('body_end');?>
 </body></html>
