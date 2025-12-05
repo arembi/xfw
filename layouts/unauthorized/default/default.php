58b029d683efe3179f648c3ff1adc5a1038dee7c
vscode <arembi@protonmail.com>
2025-12-05 09:23:43 +0100

    simplified unauthorized display

--- layouts/unauthorized/default/default.php
+++ layouts/unauthorized/default/default.php
@@ -1,4 +1,4 @@
 <?php $this->embed('user_status'); ?>
-<div style="font-size:1.5em;font-weight:bold;">
-  Permission denied.
+<div>
+	<?php $this->print(['en'=>'Permission denied.']); ?>
 </div>
--- modules/unauthorized/module.unauthorized.php
+++ modules/unauthorized/module.unauthorized.php
@@ -5,10 +5,14 @@ namespace Arembi\Xfw\Module;
 use Arembi\Xfw\Core\ModuleBase;
 
 class UnauthorizedBase extends ModuleBase {
+	
 	protected static $autoloadModel = false;
+	
 	
 	protected function init()
 	{
-		$this->autoAction(false);
+		$this
+			->autoAction(false)
+			->autoFinalize(false);
 	}
 }
