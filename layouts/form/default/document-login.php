552b167e7b69253fe6284b7c8a4c0d5b2dc3ebd5
vscode <arembi@protonmail.com>
2025-12-19 20:24:26 +0100

    fixed indentation

--- layouts/form/default/document-login.php
+++ layouts/form/default/document-login.php
@@ -4,6 +4,6 @@
 		<?php $this->print($fields['password']->label()) ?>: <?php $this->print($fields['password']->tag()) ?>
 		<?php $this->print($fields['language']->label()) ?>: <?php $this->print($fields['language']->tag()) ?>
 		<?php $this->print($fields['formId']->tag()) ?>
-	<input type="submit" value="<?php $this->print(['en'=>'Login']); ?>"/>
+		<input type="submit" value="<?php $this->print(['en'=>'Login']); ?>"/>
 	</form>
 </div>
