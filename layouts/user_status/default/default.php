cdfcbc34fd1d2786a307cc957c882642643e5a92
vscode <arembi@protonmail.com>
2025-12-19 20:24:53 +0100

    login for retrieved by name

--- layouts/user_status/default/default.php
+++ layouts/user_status/default/default.php
@@ -1,19 +1,18 @@
-<div class="user-status">
+<div>
 <?php if($user->isLoggedIn()): ?>
-  <div>
-    Hi, <?php $this->print($firstName . ' ' . $lastName . ' (' . $username . ')')?><br>
-    Group: <?php $this->print($userGroup) ?> (CL: <?php $this->print($clearanceLevel) ?>)
-  </div>
-  <div>
-    Log out: <?php $this->embed('form', ['formId' => 2]);?>
-  </div>
+	<div>
+		Hi, <?php $this->print($firstName . ' ' . $lastName . ' (' . $username . ')')?><br>
+		Group: <?php $this->print($userGroup) ?> (CL: <?php $this->print($clearanceLevel) ?>)
+	</div>
+	<div>
+		Log out: <?php $this->embed('form', ['formId' => 2]);?>
+	</div>
 <?php else: ?>
-  <div>
-    Hi, <?php $this->print($firstName)?><br>
-  </div>
-  <div>
-    <?php $this->embed('form', ['formId' => 1]);?>
-  </div>
+	<div>
+		Hi, <?php $this->print($firstName)?><br>
+	</div>
+	<div>
+		<?php $this->embed('form', ['formName' => 'login']);?>
+	</div>
 <?php endif; ?>
-
 </div>
